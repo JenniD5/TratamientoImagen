5a81985ebd4b9c2a5ef07457f8d68cc2d819bfa3
JenniD5 <elizabethduran0105@gmail.com>
2021-08-26 18:18:38 -0700

    ejercicio1

---
+++ actividad1/js/script.js
@@ -0,0 +1,21 @@
+alert ('Alerta de ejemplo de javascript');
+//definicion de variables 
+
+function suma(){
+    //let y = document.getElementById("suma")
+    var numero1 = 5;
+    var numero2 = 6;
+    var suma = numero1 +numero2;
+    console.log(suma);
+    //y = document.write(suma)    
+}
+suma();
+
+
+
+function ccolor(){
+    let x = document.getElementById("demo");
+    x.style.fontSize = "25px";
+    x.style.color="red";
+    
+}
